e964fc211a2232add4dff7e890d5da7c3d1fe701
Ben <benzuvsteel@gmail.com>
2018-07-23 20:29:45 -0700

    pushed keys

---
+++ src/keys.js
@@ -0,0 +1,5 @@
+export const consumerKey = 'Sv02CPrUTxi5G1ASmeFneZsxG';
+export const consumerSecret = 'eX6DisewEFiW37vX6ZB6V2rAZrBdu8IJwtpyQZlFXEsscachaq';
+export const accessToken = '17666466-BbCfzLlJCVLI2tIpqjPn3sFmQ0JbeNlnTkiZH6YHB';
+export const tokenSecret = 'koLyhqpVsLVlPqU26U3mvJaDWlRX8CWaSomAW5oOoh5V7';
+
